b32e4fc3df3ce37fba2af359bf1a3f80a52f14d1
Fawzi The-Programmer <fawz11gharib@gmail.com>
2022-03-27 21:29:24 +0300

    diet planner

--- lib/meal_times.dart
+++
@@ -1,8 +0,0 @@
-class MealsWidget {
-  final String title;
-  final String subtitle;
-  final String imgPath;
-
-  MealsWidget(
-      {required this.title, required this.subtitle, required this.imgPath});
-}
